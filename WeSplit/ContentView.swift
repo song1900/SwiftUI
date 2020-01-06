5001617985f2b5a19711a3de0722659fda8820aa
WooJin Song <woojin1900@gmail.com>
2020-01-06 14:42:01 +0900

    First Commit

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -6,11 +6,26 @@
 //  Copyright © 2020 viewmotion. All rights reserved.
 //
 
+
+
+/*
+ https://www.hackingwithswift.com/100/swiftui/16
+ */
 import SwiftUI
 
 struct ContentView: View {
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = 0
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Picker("Select your student", selection: $selectedStudent) {
+                ForEach(0 ..< students.count) {
+                    Text(self.students[$0])
+                }
+            }
+            Text("Your chose: Student # \(students[selectedStudent])")
+        }
     }
 }
 
